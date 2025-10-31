e35220acbb5db9599adcd854040ee7368af63459
euEmica <emica.costa1749@gmail.com>
2025-10-30 21:49:57 -0300

    fix: change duracao column to duracao_minutos

--- src/main/java/com/example/demo/models/MusicaModel.java
+++ src/main/java/com/example/demo/models/MusicaModel.java
@@ -18,7 +18,7 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "`MUSICA`")
+@Table(name = "MUSICA")
 public class MusicaModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -32,7 +32,7 @@ public class MusicaModel implements Serializable {
 
     @Column(nullable = false, unique = true)
     private String titulo;
-    @Column(nullable = false)
+    @Column(nullable = false, name="duracao_minutos")
     private Integer duracao;
 
     public UUID getId() {
